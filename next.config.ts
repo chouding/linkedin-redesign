5451aba65da6b8e72d540216b9a7e2b92c81bf60
chouding <raymond.chou523@gmail.com>
2025-06-02 19:15:01 -0700

    Header implementation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL('https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg'),
+      new URL('https://cdn4.iconfinder.com/data/icons/finite-application/16/grid-9-1024.png'),
+      new URL('https://www.svgrepo.com/show/61705/down-arrow.svg'),
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import { SearchIcon } from "lucide-react"
+
+export default function Header() {
+  return (
+    <div className="flex items-center p-2 px-6 shadow-md">
+      {/* 3 horizontal lines (hamburger menu) */}
+      <div className="mr-4" style={{ display: "flex", flexDirection: "column", justifyContent: "center", gap: "4px" }}>
+        <div style={{ width: "24px", height: "3px", background: "#333" }} />
+        <div style={{ width: "24px", height: "3px", background: "#333" }} />
+        <div style={{ width: "24px", height: "3px", background: "#333" }} />
+      </div>
+      {/* LinkedIn Icon */}
+      <Image 
+        className="rounded-xs"
+        // src="https://links.papareact.com/b3z"
+        src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg"
+        width={34.17}
+        height={34.17}
+        alt="LinkedIn Logo"
+      />
+      {/* Search Bar */}
+      <div className="flex-1 flex justify-center">
+        <form className="flex items-center bg-gray-100 rounded-sm p-2 w-[200px] sm:w-[400px] gap-2">
+          <SearchIcon 
+            size={16}
+            strokeWidth={3}
+          />
+          <input 
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent border-none focus:ring-0 focus:outline-none placeholder:text-gray-500" 
+          />
+        </form>
+      </div>
+      {/* For Business */}
+      <div className="hidden sm:inline-flex items-center space-x-2 ml-4 flex-col">
+        <Image 
+          src="https://cdn4.iconfinder.com/data/icons/finite-application/16/grid-9-1024.png"
+          width={32}
+          height={32}
+          alt="Business Icon"
+        />
+        <div className="flex space-x-1">
+          <p className="text-sm text-gray-600">
+            For Business
+          </p>
+          <Image
+            src="https://www.svgrepo.com/show/61705/down-arrow.svg"
+            width={12}
+            height={12}
+            alt="Down Arrow Icon"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}   
